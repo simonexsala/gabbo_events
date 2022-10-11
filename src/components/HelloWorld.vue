f1aa4f8c2cbcc736beb5de671b1f47754f03c225
Simone Sala <simonexsala@gmail.com>
2022-10-11 14:06:10 +0200

    new font

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,9 +24,13 @@ defineProps({
 <style scoped>
 h1 {
   font-weight: 500;
+  font-size: 2.3rem;
+  /*
   font-size: 2.6rem;
+  */
   top: -10px;
   margin-top: 1rem;
+  font-family: 'Monoton', cursive;
 }
 
 h3 {
