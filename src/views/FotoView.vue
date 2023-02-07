d99346f2d32ad377add7d262449c849fe1d19fbe
Simone Sala <simonexsala@gmail.com>
2023-02-07 20:14:44 +0100

    added photos

--- src/views/FotoView.vue
+++ src/views/FotoView.vue
@@ -3,7 +3,7 @@
     <div class="evento">
       <h1 align="center">Foto eventi</h1>
       <h2 align="center">
-        <a target="_blank" href="https://we.tl/t-DaKIgwPzJo"><i>{{ $event.title }} &nbsp;</i>
+        <a target="_blank" href="https://wetransfer.com/downloads/0e5fed2422adc9a5005ca781c3dd83c920230205193428/f43ea8"><i>{{ $event.title }} &nbsp;</i>
           <svg height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
             <path d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z" />
             <path d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z" />
@@ -11,7 +11,7 @@
         </a>
       </h2>
     </div>
-    <a class="imageLink" target="_blank" href="https://we.tl/t-DaKIgwPzJo">
+    <a class="imageLink" target="_blank" href="https://wetransfer.com/downloads/0e5fed2422adc9a5005ca781c3dd83c920230205193428/f43ea8">
       <img class="image" src="../../public/eventi/foto.jpg" alt="evento">
     </a>
   </div>
